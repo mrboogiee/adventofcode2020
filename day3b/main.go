b27780c57792dc9771bbe9be5f2c973727c3e21b
Johan Bogema <johan.bogema@on2it.net>
2022-10-11 16:51:21 +0200

    new inputs and fixed day3b

--- day3b/main.go
+++ day3b/main.go
@@ -29,26 +29,24 @@ func main() {
 	scenario3 := processSlope(5, 1, inputs)
 	scenario4 := processSlope(7, 1, inputs)
 	scenario5 := processSlope(1, 2, inputs)
-	fmt.Printf("Amount of trees: %d\n", scenario1*scenario2*scenario3*scenario4*scenario5)
+	fmt.Printf("Amount of trees Day 3a: %d\n", processSlope(3, 1, inputs))
+	fmt.Printf("Amount of trees Day 3b: %d\n", scenario1*scenario2*scenario3*scenario4*scenario5)
 }
 
 func processSlope(right, down int, inputs []string) int {
-	var currentX = 0
-	var currentY = 0
+	var currentX int
+	var currentY int
+	// var newCounter int
 	var amountOfTrees int
 	for j, row := range inputs {
 		if j == currentY {
-			for i, item := range row {
-				if i == currentX {
-					if item == '#' {
-						amountOfTrees++
-					}
-					currentX += right
-					if currentX > 30 {
-						currentX = currentX - 31
-					}
-					break
-				}
+			if row[currentX] == '#' {
+				fmt.Printf("%d %d\n", currentY, currentX)
+				amountOfTrees++
+			}
+			currentX += right
+			if currentX >= len(row) {
+				currentX = currentX - len(row)
 			}
 			currentY += down
 		}
